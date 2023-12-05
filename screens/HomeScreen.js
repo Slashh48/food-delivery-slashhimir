40a1535aa35080f190ae787eafa51c12368f0cad
Slashh48 <pablodiablofun@gmail.com>
2023-12-05 22:11:27 +0100

    Idemo polako ali sigurno, native je jos veca glavobolja ali brate tu je Sani burazengija da dize nivo energije LEZZ GOOOUUU BRAAAAAAATTTTEEEEEE

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  UserCircleIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text className="text-red-600">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={require("../images/jesus.png")}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current location
+            <ChevronDownIcon size={20} color="#0099cc" />
+          </Text>
+        </View>
+
+        <UserCircleIcon size={35} color="#0099cc" />
+      </View>
+    </SafeAreaView>
   );
 };
 
